25c059ba87fa8904512366613325ab93d6192395
josuerushanika <josuerushanika@gmail.com>
2023-11-09 04:59:29 +0200

    Update files

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -1,14 +1,22 @@
 import { View, Text, SafeAreaView, StatusBar, Platform } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Home from "./screens/Home"
+
+const Stack = createNativeStackNavigator()
 
 const Main = () => {
-  return (
-    <View>
-      < SafeAreaView style={{ paddingVertical: Platform.OS=== "android"? StatusBar.currentHeight : 0 }}>
-         <Text>Main</Text>
-      </ SafeAreaView>
-    </View>
-  )
+  return <NavigationContainer>
+     <Stack.Navigator >
+      <Stack.Group>
+                 < Stack.Screen name="home" component={Home}/>
+      </Stack.Group>
+     </Stack.Navigator>
+
+  </NavigationContainer>
+   
+  
 }
 
 export default Main
