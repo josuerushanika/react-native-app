893cdf07253d5e2e096ee32e287ddc819084b602
josuerushanika <josuerushanika@gmail.com>
2023-11-12 23:46:39 +0200

    Update files

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import { Image } from 'react-native';
 import React from "react";
 import { colors } from "../styles/styles";
 import { Button } from "react-native-paper";
