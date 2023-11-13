c37558e15fab9f3ef9e080309f4a6d192583dd5e
josuerushanika <josuerushanika@gmail.com>
2023-11-13 22:20:17 +0200

    Update some package

--- frontend/screens/ProductDetails.jsx
+++ frontend/screens/ProductDetails.jsx
@@ -1,17 +1,29 @@
-import { View, Text, Dimensions,  StyleSheet } from "react-native";
+import { View, Text, Dimensions, StyleSheet,Image } from "react-native";
 import React, { useRef } from "react";
 import { colors, defaultStyle } from "../styles/styles";
 import Header from "../components/Header";
 import Carousel from "react-native-snap-carousel";
 
-const SLIDER_WIDTH =  Dimensions.get("window").width;
+const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = SLIDER_WIDTH;
-
 
 const ProductDetails = ({ route: { params } }) => {
   console.log(params.id);
 
   const isCarousel = useRef(null);
+
+  const images = [
+    {
+      id: "aakakakakakaka",
+      url: "djdjdjoowowsjsjsjxnxnxnxnxxnxnx",
+    },
+    {
+        id: "aakakakakakaka",
+        url: "djdjdjoowowsjsjsjxnxnxnxnxxnxnx",
+    },
+
+  ];
+
   return (
     <View
       style={{
@@ -26,29 +38,29 @@ const ProductDetails = ({ route: { params } }) => {
 
       <Carousel
         layout="stack"
-        sliderWidth={SLIDER - WIDTH}
+        sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         ref={isCarousel}
-        data= {images}
+        data={images}
         renderItem={CarouselCardItem}
-         />
+      />
       <Text>ProductDetails</Text>
     </View>
   );
 };
 
-const CarouselCardItem = () => {
-   <View style={style.container}>
-     <Image source={{uri: item.url}} style={style.image}/>
-   </View>
-}
+const CarouselCardItem = (item, index) => (
+  <View style={style.container}>
+    <Image source={{ uri: item.url }} style={style.image} />
+  </View>
+);
 
-const style = StyleSheet.create ({
-    container : {},
-    image: {
-        width: ITEM_WIDTH,
-        resizeMode: "contain",
-        height: 250,
-    }
-})
+const style = StyleSheet.create({
+  container: {},
+  image: {
+    width: ITEM_WIDTH,
+    resizeMode: "contain",
+    height: 250,
+  },
+});
 export default ProductDetails;
